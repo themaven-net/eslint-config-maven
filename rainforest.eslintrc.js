ff4235a4681b5d632ea28690275ac983f13ac680
Joe Wright <joe@maven.io>
2021-09-01 14:49:38 -0700

    [MEMBER-1349] add eslint and prettier config files for rainforest

---
+++ rainforest.eslintrc.js
@@ -0,0 +1,159 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true
+  },
+  extends: ['prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    sourceType: 'module'
+  },
+  plugins: ['eslint-plugin-import', '@typescript-eslint'],
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    '@typescript-eslint/adjacent-overload-signatures': 'error',
+    '@typescript-eslint/array-type': [
+      'error',
+      {
+        default: 'array'
+      }
+    ],
+    '@typescript-eslint/ban-types': 'off',
+    '@typescript-eslint/consistent-type-assertions': 'error',
+    '@typescript-eslint/dot-notation': 'error',
+    '@typescript-eslint/indent': 'off',
+    '@typescript-eslint/member-delimiter-style': [
+      'off',
+      {
+        multiline: {
+          delimiter: 'none',
+          requireLast: true
+        },
+        singleline: {
+          delimiter: 'semi',
+          requireLast: false
+        }
+      }
+    ],
+    '@typescript-eslint/naming-convention': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/no-empty-interface': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-misused-new': 'error',
+    '@typescript-eslint/no-namespace': 'error',
+    '@typescript-eslint/no-parameter-properties': 'off',
+    '@typescript-eslint/no-shadow': [
+      'error',
+      {
+        hoist: 'all'
+      }
+    ],
+    '@typescript-eslint/no-this-alias': 'error',
+    '@typescript-eslint/no-unused-expressions': 'error',
+    '@typescript-eslint/no-use-before-define': 'off',
+    '@typescript-eslint/no-var-requires': 'error',
+    '@typescript-eslint/prefer-for-of': 'error',
+    '@typescript-eslint/prefer-function-type': 'error',
+    '@typescript-eslint/prefer-namespace-keyword': 'error',
+    '@typescript-eslint/quotes': [
+      'error',
+      'single',
+      {
+        avoidEscape: true
+      }
+    ],
+    '@typescript-eslint/semi': ['off', null],
+    '@typescript-eslint/triple-slash-reference': [
+      'error',
+      {
+        path: 'always',
+        types: 'prefer-import',
+        lib: 'always'
+      }
+    ],
+    '@typescript-eslint/type-annotation-spacing': 'off',
+    '@typescript-eslint/unified-signatures': 'error',
+    'arrow-parens': ['off', 'always'],
+    'brace-style': ['off', 'off'],
+    'comma-dangle': 'off',
+    complexity: 'off',
+    'constructor-super': 'error',
+    'dot-notation': 'error',
+    'eol-last': 'off',
+    eqeqeq: ['error', 'smart'],
+    'guard-for-in': 'error',
+    'id-blacklist': ['error', 'any', 'Number', 'String', 'string', 'Boolean', 'Undefined', 'undefined'],
+    'id-match': 'error',
+    'import/no-extraneous-dependencies': 'off',
+    'import/no-internal-modules': 'off',
+    'import/order': 'off',
+    indent: 'off',
+    'linebreak-style': 'off',
+    'max-classes-per-file': ['error', 1],
+    'max-len': 'off',
+    'new-parens': 'off',
+    'newline-per-chained-call': 'off',
+    'no-bitwise': 'off',
+    'no-caller': 'error',
+    'no-cond-assign': 'error',
+    'no-debugger': 'error',
+    'no-duplicate-case': 'error',
+    'no-duplicate-imports': 'error',
+    'no-empty': 'off',
+    'no-empty-function': 'off',
+    'no-eval': 'error',
+    'no-extra-bind': 'error',
+    'no-extra-semi': 'off',
+    'no-fallthrough': 'off',
+    'no-invalid-this': 'off',
+    'no-irregular-whitespace': 'off',
+    'no-multiple-empty-lines': 'off',
+    'no-new-func': 'error',
+    'no-new-wrappers': 'error',
+    'no-redeclare': 'error',
+    'no-return-await': 'error',
+    'no-sequences': 'error',
+    'no-shadow': 'off',
+    'no-sparse-arrays': 'error',
+    'no-template-curly-in-string': 'error',
+    'no-throw-literal': 'error',
+    'no-trailing-spaces': 'off',
+    'no-undef-init': 'error',
+    'no-underscore-dangle': 'error',
+    'no-unsafe-finally': 'error',
+    'no-unused-expressions': 'error',
+    'no-unused-labels': 'error',
+    'no-use-before-define': 'off',
+    'no-var': 'error',
+    'object-shorthand': 'error',
+    'one-var': ['error', 'never'],
+    'padded-blocks': [
+      'off',
+      {
+        blocks: 'never'
+      },
+      {
+        allowSingleLineBlocks: true
+      }
+    ],
+    'prefer-arrow/prefer-arrow-functions': 'off',
+    'prefer-const': 'error',
+    'prefer-object-spread': 'error',
+    'quote-props': 'off',
+    radix: 'error',
+    semi: 'off',
+    'space-before-function-paren': 'off',
+    'space-in-parens': ['off', 'never'],
+    'spaced-comment': [
+      'error',
+      'always',
+      {
+        markers: ['/']
+      }
+    ],
+    'use-isnan': 'error',
+    'valid-typeof': 'off'
+  }
+}
