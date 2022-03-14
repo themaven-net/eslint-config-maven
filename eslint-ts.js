242ee5ef12e63066354055fefad26427ee8fff55
Yonathan Randolph <yonathan.randolph@maven.io>
2022-03-14 15:49:55 -0700

    Enable parts of eslint:recommended
    Enable a high-precision subset of eslint:recommended that have few false positives. There are other useful rules (e.g. no-unused-vars, no-undef) but they probably require more effort to enable

--- eslint-ts.js
+++ eslint-ts.js
@@ -1,5 +1,9 @@
 module.exports = {
-    "extends": "maven",
+    "extends": [
+        "maven",
+        // disable eslint rules that conflict with @typescript-eslint
+        'plugin:@typescript-eslint/eslint-recommended',
+    ],
     "env": {
         "node": true
     },
@@ -9,5 +13,16 @@ module.exports = {
         "ecmaVersion": 2020,
         "project": ["./tsconfig.json"]
     },
-    "rules": {}
+    "rules": {
+        // Disable opinionated rules from plugin:@typescript-eslint/eslint-recommend
+        // https://github.com/typescript-eslint/typescript-eslint/blob/v5.15.0/packages/eslint-plugin/src/configs/eslint-recommended.ts
+        'no-var': 'warn',
+        'prefer-const': 'warn',
+        'prefer-rest-params': 'warn',
+        'prefer-spread': 'warn',
+
+        // Enable typescript version of rules originally from eslint
+        // https://github.com/typescript-eslint/typescript-eslint/tree/v5.15.0/packages/eslint-plugin/docs/rules#extension-rules
+        '@typescript-eslint/no-dupe-class-members': 'error',
+    },
 }
--- index.js
+++ index.js
@@ -23,6 +23,29 @@ module.exports = {
             'error',
             require('./.prettierrc'),
         ],
+
+        // enable a subset of eslint:recommended; consider enabling all of it
+        // https://eslint.org/docs/rules/
+        // When adding a rule here, also check whether you have to add
+        // the typescript version in eslint-ts.js
+        'getter-return': 'error',
+        'no-async-promise-executor': 'error',
+        'no-compare-neg-zero': 'error',
+        'no-const-assign': 'error',
+        'no-this-before-super': 'error',
+        'no-class-assign': 'error',
+        'no-dupe-args': 'error',
+        'no-dupe-class-members': 'error',
+        'no-dupe-else-if': 'error',
+        'no-dupe-keys': 'error',
+        'no-duplicate-case': 'error',
+        'no-fallthrough': 'error',
+        'no-func-assign': 'error',
+        'no-import-assign': 'error',
+        'no-setter-return': 'error',
+        'no-sparse-arrays': 'error',
+        'use-isnan': 'error',
+
         'one-var': 'off',
         'spaced-comment': 'off',
         'no-useless-escape': 'off', // TODO should probably be on, with our errors fixed
